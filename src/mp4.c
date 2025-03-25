a7cda4f5205fb079c3569c7c435db63947f8a871
kashhash <kashyapj@apra.in>
2025-03-25 17:20:40 +0400

    Add free for avccData

--- src/mp4.c
+++ src/mp4.c
@@ -101,8 +101,10 @@ void mp4_video_decoder_config_destroy(struct mp4_video_decoder_config *vdc)
 	case MP4_VIDEO_CODEC_AVC:
 		free(vdc->avc.sps);
 		free(vdc->avc.pps);
+		free(vdc->avc.avccData);
 		vdc->avc.sps = NULL;
 		vdc->avc.pps = NULL;
+		vdc->avc.avccData = NULL;
 		break;
 	case MP4_VIDEO_CODEC_HEVC:
 		free(vdc->hevc.vps);
